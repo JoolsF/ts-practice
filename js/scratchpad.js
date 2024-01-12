42728e410ec01b46746830a2f3dc74913b48d025
Julian Fenner <joolsmf@gmail.com>
2024-01-12 17:49:22 +0000

    update

---
+++ js/scratchpad.js
@@ -0,0 +1,11 @@
+const add = (a, b) => {
+	return a + b
+}
+let x = add(4, 6)
+
+
+// to run in the browser
+// document.addEventListener("DOMContentLoaded", function() {
+//     var result = document.getElementById("scratchpadText");
+//     result.textContent = "foo bar"; // write output
+// });
