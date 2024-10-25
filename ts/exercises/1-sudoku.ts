dbab1fa5d7cc8f126d96b94bbf2b78eebadac6c6
Julian Fenner <joolsmf@gmail.com>
2024-10-25 23:16:04 +0100

    Add square function

--- ts/exercises/1-sudoku.ts
+++ ts/exercises/1-sudoku.ts
@@ -1,3 +1,6 @@
+type validNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+// implement array range with Array.from
 function fillArraySequential(n: number) {
     const array: number[] = []
     for (let i = 0; i < n; i++) {
@@ -10,7 +13,7 @@ function fillArraySequential(n: number) {
 const array = fillArraySequential(81)
 
 // 1 - 9
-function row(n: number) {
+function row(n: validNumber) {
     const nMinus = n// - 1
     return array.slice(nMinus, nMinus + 9)
 }
@@ -19,7 +22,7 @@ function row(n: number) {
 /*
  1,2,3,4,5,6,7,8,9
 */
-function column(n: number) {
+function column(n: validNumber) {
     // set limit for start
     // set acceptable values
     const result: number[] = []
@@ -33,7 +36,29 @@ function column(n: number) {
     return result;
 }
 
-const res = column(9)
+//TODO express this more elegantly
+function square(n: validNumber) {
+    const indices: number[] = []
+    switch(n) {
+        case 1: return  [0, 1, 2, 9,  10, 11, 18, 19, 20];
+        case 2: return  [3, 4, 5, 12, 13, 14, 21, 22, 23];
+        case 3: return  [6, 7, 8, 15, 16, 17, 24, 25, 26];
+        case 4: return  [27, 28, 29, 36, 37, 38, 45, 46, 47];
+        case 5: return  [30, 31, 32, 39, 40, 41, 48, 49, 50];
+        case 6: return  [33, 34, 35, 42, 43, 44, 51, 52, 53];
+        case 7: return  [54, 55, 56, 63, 64, 65, 72, 73, 74];
+        case 8: return  [57, 58, 59, 66, 67, 68, 75, 76, 77];
+        case 9: return  [60, 61, 62, 69, 70, 71, 78, 79, 80];
+        default: return [];
+    }  
+   
 
-// console.log(array)
-console.log(res)
+    const res = indices
+        .map(i => i * n)
+        .map(i => array[i])
+
+    return res
+
+}
+
+
