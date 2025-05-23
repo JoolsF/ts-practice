9bc40e477e38d4dcc6c4993d3ae6355e222e80f4
Julian Fenner <joolsmf@gmail.com>
2025-05-23 18:17:20 +0100

    update

--- notes/scratchpad.ts
+++ notes/scratchpad.ts
@@ -1,2 +1,14 @@
+const a = {
+  id: { S: "PAYMENT_ID" },
+  entityType: { S: "PAYMENT" },
+  sortKey: { S: "PAYMENT#SERVICE#EXTERNAL_ID#OTHER" },
+  accountId: { S: "ACCOUNT_ID" },
+  paymentDisplayIdentification: {
+    M: {
+      value: { S: "ID_VALUE" },
+      description: { S: "ID_DESC" },
+    }
+  }
+}
 
-
+console.log(a.sortKey?.S!.startsWith('PAYMENT'))
